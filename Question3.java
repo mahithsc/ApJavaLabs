7493bee1df2140a9f6690e5bcb9a1bf633471f9c
Tekerz99 <47924895+Tekerz99@users.noreply.github.com>
2021-01-16 12:37:56 -0500

    Create Question3.java

---
+++ Question3.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Question3{
+    public static void main(String []args)
+    {
+        String name;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("What is your name? ");
+        name = sc.nextLine();
+        int space = name.indexOf(" ");
+        System.out.println(space);
+        
+
+
+
+    }
+
+}
+
+// = assignment opperator
